bb03c455d38c16be1be1470fbc49d16c1ee58f02
Scott Lawrence <bytbox@gmail.com>
2010-07-20 22:10:14 -0400

    logging system

--- main.go
+++ main.go
@@ -9,18 +9,21 @@ import (
 	"os"
 )
 
-var version = '0.0.1'
+var version = "0.0.1"
 
 var opts struct {
 	showVersion *bool
+	verbose *bool
 }
 
 func main() {
 	// Option definitions
 	opts.showVersion = flag.Bool("version", 
-			false, 
-			"display version information and exit")
-
+		false,
+		"display version information and exit")
+	opts.verbose = flag.Bool("verbose",
+		false,
+		"use verbose output")
 	// Do the argument parsing
 	flag.Parse()
 	if *opts.showVersion {
@@ -28,9 +31,21 @@ func main() {
 		os.Exit(0)
 	}
 
-	fmt.Printf("see!")
 }
 
+// Show version information
 func ShowVersion() {
 	fmt.Printf("golint v%s\n",version)
+}
+
+// Log output in an appropriate fashion, depending on settings
+func Message(msg string) {
+	fmt.Printf("%s\n",msg)
+}
+
+// Log verbose output the right way, depending on settings
+func Verbose(msg string) {
+	if *opts.verbose {
+		fmt.Printf("%s\n",msg)
+	}
 }
