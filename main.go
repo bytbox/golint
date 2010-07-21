57333003aa2aa2e7a96c4a796e80f388bbb9c6e4
Scott Lawrence <bytbox@gmail.com>
2010-07-20 20:45:21 -0400

    parsing flags and showing version information

--- main.go
+++ main.go
@@ -3,6 +3,34 @@
 */
 package main
 
-func main() {
+import (
+	"fmt"
+	"flag"
+	"os"
+)
 
+var version = '0.0.1'
+
+var opts struct {
+	showVersion *bool
+}
+
+func main() {
+	// Option definitions
+	opts.showVersion = flag.Bool("version", 
+			false, 
+			"display version information and exit")
+
+	// Do the argument parsing
+	flag.Parse()
+	if *opts.showVersion {
+		ShowVersion()
+		os.Exit(0)
+	}
+
+	fmt.Printf("see!")
+}
+
+func ShowVersion() {
+	fmt.Printf("golint v%s\n",version)
 }
