8af023dacca90de5a74040faa0e08c2f64fa7609
Scott Lawrence <bytbox@gmail.com>
2010-08-09 16:25:49 -0400

    [NEW] added TODO, FIXME, and XXX stateless lints

---
+++ comments.go
@@ -0,0 +1,36 @@
+package main
+
+import "strings"
+
+// TodoLint is a stateless linter that checks for and prints out lines that
+// have TODO notices.
+type TodoLint struct {}
+
+func (TodoLint) Lint(line string) (msg string, err bool) {
+	if strings.Index(line, "TODO") != -1 {
+		msg, err = line, true
+	}
+	return
+}
+
+// FixmeLint is a stateless linter that checks for and prints out lines that
+// have FIXME notices.
+type FixmeLint struct {}
+
+func (FixmeLint) Lint(line string) (msg string, err bool) {
+	if strings.Index(line, "FIXME") != -1 {
+		msg, err = line, true
+	}
+	return
+}
+
+// XXXLint is a stateless linter that checks for and prints out lines that
+// have XXX-style TODO notices (a convention in Java, at least).
+type XXXLint struct {}
+
+func (XXXLint) Lint(line string) (msg string, err bool) {
+	if strings.Index(line, "XXX") != -1 {
+		msg, err = line, true
+	}
+	return
+}
