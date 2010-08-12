10889fa707a76393ac3b999e2977e0356fbe5833
Scott Lawrence <bytbox@gmail.com>
2010-08-12 03:52:49 -0400

    PackageDeprecationLint stub

--- deprecation.go
+++ deprecation.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
 	"regexp"
 )
 
 // DeprecationLint is a stateless linter that looks for and reports deprecated
 // constructs. New DebrecationLints should be created with NewDeprecationLint
 // or a related method.
+//
+// Not all deprecated forms may be expressable with a DeprecationLint.
 type DeprecationLint struct {
-	Form string // a regular expression representing the form
+	Form   string // a regular expression representing the form
 	Reason string // the reason for deprecation, or an alternative form
 }
 
@@ -29,5 +32,21 @@ func (l DeprecationLint) Lint(line string) (msg string, err bool) {
 		// XXX once the regexp patch is out, we can give more info
 		msg = fmt.Sprintf("deprecated: %s", l.Reason)
 	}
+	return
+}
+
+// PackageDeprecationLint is a parsing linter that looks for and reports usage
+// of deprecated packages.
+type PackageDeprecationLint struct {
+	Package string // the name of the deprecated package
+	Reason  string // the reason for deprecation, or an alternative form
+	file    *ast.File
+}
+
+func (l PackageDeprecationLint) Init(file *ast.File) {
+	l.file = file
+}
+
+func (l PackageDeprecationLint) Next() (msg string, err bool) {
 	return
 }
