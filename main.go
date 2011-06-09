61b174257da712ebcc73f5acd32a1c5e39f84006
Scott Lawrence <bytbox@gmail.com>
2011-06-09 02:06:44 -0400

    more TODOs

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 // TODO check for (potentially) overlapping imports
 // TODO pattern-matching in ParsingLinter
 // TODO use actual lints in checking for valid parse
+// TODO some sort of pragma to disable golint checking for a file or line
 
 var version = "0.1.0"
 
