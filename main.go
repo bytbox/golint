f0d7de4550243df0746f8469f7f97aed59e4b0de
Scott Lawrence <bytbox@gmail.com>
2012-02-19 10:18:55 -0500

    Scanning directory

--- main.go
+++ main.go
@@ -1,5 +1,76 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+	p "path/filepath"
+	"strings"
+)
 
+var version = `0.3.0`
+
+var (
+	verbose     = flag.Bool("v", false, "verbose output")
+	showversion = flag.Bool("version", false, "show version information")
+)
+
+func verb(f string, a ...interface{}) {
+	if *verbose {
+		fmt.Fprintf(os.Stderr, f, a...)
+	}
+}
+
+func main() {
+	flag.Parse()
+	files := flag.Args()
+
+	if *showversion {
+		fmt.Printf("golint %s\n", version)
+		return
+	}
+
+	verb("Scanning for source files... ")
+	if len(files) == 0 {
+		// just use the current directory if no files were specified
+		files = make([]string, 1)
+		files[0] = "."
+	}
+
+	srcs := make([]string, 0)
+	for _, fname := range files {
+		srcs = append(srcs, listFiles(fname, ".go")...)
+	}
+	verb("\n")
+}
+
+func listFiles(fname string, suf string) (fs []string) {
+	info, err := os.Stat(fname)
+	if err != nil {
+		return
+	}
+	if info.IsDir() {
+		f, _ := os.Open(fname)
+		dn, _ := f.Readdirnames(-1)
+		for _, filename := range dn {
+			if filename[0] == '.' {
+				continue
+			}
+			filename = p.Join(fname, filename)
+			info, err := os.Stat(filename)
+			if err != nil {
+				continue
+			}
+			if info.IsDir() {
+				fs = append(fs, listFiles(filename, suf)...)
+			} else if strings.HasSuffix(filename, suf) {
+				verb("%s ", filename)
+				fs = append(fs, filename)
+			}
+		}
+	} else if strings.HasSuffix(fname, suf) {
+		verb("%s", fname)
+		fs = append(fs, fname)
+	}
+	return
 }
